544d79be791ace862d4e6561f9c2db09a0024f95
Sean Fong <seancfong@gmail.com>
2019-04-01 15:24:16 -0700

    Add circle slider

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Canvas Setup
 var canvas = document.querySelector("canvas");
 
-canvas.height = window.innerHeight - 30;
+canvas.height = window.innerHeight - 130;
 canvas.width = window.innerWidth - 30;
 
 var c = canvas.getContext("2d");
 
 var circles = [];
 
-var circleCount = 12;
+var slider = document.getElementById("circleSlider");
+
+var circleCount = slider.value;
 
 const radianInc = 0.02;
+
+var radianCount = 0;
 
 function Circle(relX, relY, dx, dy, radius, radians, radianOrientation, distFromCenter) {
   this.x = relX;
@@ -38,8 +42,9 @@ function Circle(relX, relY, dx, dy, radius, radians, radianOrientation, distFrom
 }
 
 function init() {
+  circles = [];
   for (var i = 0; i < circleCount; i++) {
-    circles.push(new Circle(200, 200, 0, 0, 10, (Math.PI / circleCount) * i, (Math.PI / circleCount) * i, 250));
+    circles.push(new Circle(-100, -100, 0, 0, 10, radianCount + (Math.PI / circleCount) * i, (Math.PI / circleCount) * i, 250));
     console.log(circles[i].radianOrientation);
   }
 }
@@ -48,11 +53,27 @@ function animate() {
   requestAnimationFrame(animate);
   // console.log("foo");
   c.clearRect(0, 0, canvas.width, canvas.height);
+
+  // center circle
   c.beginPath();
   c.strokeStyle = "rgb(256,256,256)";
   c.lineWidth = 5;
   c.arc(canvas.width / 2, canvas.height / 2, circles[1].distFromCenter + (circles[1].radius / 2) + 5, 0, Math.PI * 2, false);
   c.stroke();
+
+  c.font = "30px Arial";
+  c.textAlign = "center";
+  c.fillText(circleCount, canvas.width / 2, canvas.height / 2);
+
+  radianCount += radianInc;
+
+  if (slider.value != circleCount) {
+    console.log("changing");
+    circleCount = slider.value;
+    init();
+  }
+
+  // update all circles
   for (var i = 0; i < circleCount; i++) {
     circles[i].update();
   }
